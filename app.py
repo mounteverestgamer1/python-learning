151650ed465df7f888ab40d132966f17dc0f3256
mounteverestgamer1 <116048738+mounteverestgamer1@users.noreply.github.com>
2022-10-19 06:59:13 +0545

    main

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-name =input('enter your name')
-print('name')
+secret_number = 9
+guess_limit = 3
+guess_count = 0
+while guess_count < guess_limit:
+    guess = int(input('enter your guess number : '))
+    guess_count +=1
+    if guess == secret_number :
+        print('you won')
+        break
+else :
+    print('you lose')
+
+        
