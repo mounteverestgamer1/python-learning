2190909faded82ef630749e236167e583d50c779
mounteverestgamer1 <116048738+mounteverestgamer1@users.noreply.github.com>
2022-10-20 20:11:59 +0545

    greeting

---
+++ greeting.py
@@ -0,0 +1,8 @@
+def greet_user (First_name, Last_name) :
+    print(f'Hi {First_name} {Last_name} !')
+    print('Welcome Aboard')
+    
+    
+print("Start")
+greet_user("Aayush","Bishwakarma")
+print("Finish")
